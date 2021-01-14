7f4565bae1ca19943ac079af339747ce16442d7e
Josh Baker <joshuabr1234@yahoo.com>
2021-01-14 05:11:38 -0600

    update to entire project

---
+++ DeepDivePropsState/src/components/journals/journal_entry.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const JournalEntry = props => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
+    );
+};
+
+export const SomeOtherFunctionalComponent = props => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
+    );
+};
