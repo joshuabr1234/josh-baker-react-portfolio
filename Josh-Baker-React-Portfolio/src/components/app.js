39462c0bb7b9156f5dd5a57fb6c2d90bbe81abe0
Josh Baker <joshuabr1234@yahoo.com>
2021-01-14 05:18:09 -0600

    another commit

--- Josh-Baker-React-Portfolio/src/components/app.js
+++ Josh-Baker-React-Portfolio/src/components/app.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+
+import React, { Component } from "react";
 import moment from "moment";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>This is my awesome React App</h1>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+      <div className="app">
+        <h1>Josh Baker React Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
--- Josh-Baker-React-Portfolio/src/components/portfolio/portfolio-container.js
+++ Josh-Baker-React-Portfolio/src/components/portfolio/portfolio-container.js
@@ -1,35 +1,47 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio!",
-            data: [
-                {title: "Quip" },
-                {title: "Eventbrite" },
-                {title: "Ministry Safe" }, 
-                {title: "Swing Away" },
-            ]
-        };
-    }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        { title: "Quip" },
+        { title: "Eventbrite" },
+        { title: "Ministry Safe" },
+        { title: "SwingAway" }
+      ]
+    };
 
-    portfolioItems() { 
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
-        });
-    }
-    render() {
-        return (
-            <div>
-                <h2> {this.state.pageTitle} </h2>
+    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+  }
 
-                {this.portfolioItems()}
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
 
-            </div>
-        )
-    }
+  handlePageTitleUpdate() {
+    this.setState({
+      pageTitle: "Something Else"
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        {this.portfolioItems()}
+
+        <hr />
+
+        <button onClick={this.handlePageTitleUpdate}>Change Title</button>
+      </div>
+    );
+  }
 }
